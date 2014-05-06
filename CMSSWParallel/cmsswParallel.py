0e02a7ae4ba5e972343c04ed9f0c08057fc4004b
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-05-06 12:28:54 +0200

    int args

--- CMSSWParallel/cmsswParallel.py
+++ CMSSWParallel/cmsswParallel.py
@@ -59,8 +59,8 @@ if __name__ == "__main__":
   #commandLine parsing
   parser = argparse.ArgumentParser()
   parser.add_argument('--cfgFileName',required=True,help='cfg file which should be executed in parallel')
-  parser.add_argument('--numProcesses',default=2,help='number of processes which run parallel')
-  parser.add_argument('--numJobs',default=2,help='number of jobs in total')
+  parser.add_argument('--numProcesses',default=2,type=int,help='number of processes which run parallel')
+  parser.add_argument('--numJobs',default=2,type=int,help='number of jobs in total')
   parser.add_argument('--where',default='',help='where should it be executed and the output stored')
   parser.add_argument('--usage',action='store_true',default=False,help='help message')
   parser.add_argument('--debug',action='store_true',default=False,help='debugging mode')
@@ -70,6 +70,6 @@ if __name__ == "__main__":
     sys.exit(0)
   if args.debug:
     print sys.argv
-  pR = parallelRunner(args.cfgFileName,args.numProcesses,args.numJobs,args.where,args.debug)
+  pR = parallelRunner(args.cfgFileName,int(args.numProcesses),int(args.numJobs),args.where,args.debug)
   pR.createCfgs()
   pR.runParallel()
