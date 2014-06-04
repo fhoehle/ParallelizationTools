25b9e975bea0a6cbb9625beb683a06b8ca35991a
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-06-04 15:30:13 +0200

    better exitcode treament and json log

--- CMSSWParallel/cmsswParallel.py
+++ CMSSWParallel/cmsswParallel.py
@@ -17,6 +17,7 @@ class parallelRunner(object):
     cfgFile.close()
     process=cfgFileLoaded.process
     ## adapting input modules
+    logDict = {}
     inputFiles=process.source.fileNames.value()
     newNumInputFiles = max([len(inputFiles)/self.numJobs , 1])
     newInputFilesList =  [inputFiles[i:i+newNumInputFiles] for i in range(0, len(inputFiles), newNumInputFiles)]
@@ -26,17 +27,25 @@ class parallelRunner(object):
     cfgFileList=[]
     
     for job,inputFs in enumerate(newInputFilesList):
+      jobKey = 'job_'+str(job)
+      logDict[jobKey] = {'num':job}
       regexCfgFile = re.match('([^\.]*)(\.[^\.]*)',self.cfgFileName)
       newCfgFileName=regexCfgFile.group(1)+"_"+str(job)+regexCfgFile.group(2)
+      logDict[jobKey]['cfg']=newCfgFileName;
       #setting source inputFile names
       process.source.fileNames.setValue(inputFs)
+      logDict[jobKey]['inputFiles']=inputFs
       #setting file names for output modules
+      logDict[jobKey]['output'] = {}
       for outItem in process.outputModules.iteritems():
-        outItem[1].fileName.setValue(outputMods[outItem[0]].group(1)+"_"+str(job)+outputMods[outItem[0]].group(2))
+        newOutputName = outputMods[outItem[0]].group(1)+"_"+str(job)+outputMods[outItem[0]].group(2)
+        outItem[1].fileName.setValue(newOutputName)
+        logDict[jobKey]['output'][outItem[0]] = newOutputName
       cfgFileList.append(newCfgFileName)
       f = open(newCfgFileName, 'w')
       f.write(process.dumpPython())
       f.close()
+      self.logDict = logDict
     ## do it parallel
     self.jobFileName=regexCfgFile.group(1)+"_jobs.txt"
     jobFile = open(self.jobFileName,'w')
@@ -56,11 +65,23 @@ class parallelRunner(object):
     subPStdOut = [];exitCode=None
     subPr = subprocess.Popen([command],bufsize=1 , stdin=open(os.devnull),shell=True,stdout=(open(self.stdoutTMPfile,'w') if hasattr(self,'stdoutTMPfile') and self.stdoutTMPfile else subprocess.PIPE ),env=os.environ)
     #statusOutput = commands.getstatusoutput(os.getenv('CMSSW_BASE')+"/ParallelizationTools/BashParallel/doWhatEverParallel.py --jobFile "+self.jobFileName+" --numProcesses "+str(self.numProcesses)+" >& "+logFile)
+    import re
+    crabExitCode = None
     for i,line in enumerate(iter(  subPr.stdout.readline ,stopKey+'\n')):
       if 'returnCodeCrab' in line:
-        crabExitCode=line
+        reExitcode = re.match('^.*returnCodeCrab:\ ([0-9][0-9]*)\ *.*$',line)
+        if reExitcode and len(reExitcode.groups()) > 0:
+          crabExitCode = reExitcode.group(1)
+        else:
+          print "bad exitCode:\n  ","  \n".join(subPStdOut[-6:])
       subPStdOut.append(line)
-    return " ".join(subPStdOut)
+    self.logDict['exitCode']=crabExitCode
+    import json
+    self.jsonLogFileName = self.cfgFileName.strip()+'_runParallellog_JSON.txt'
+    print  self.jsonLogFileName
+    with open(self.jsonLogFileName,'w') as jsonLog:
+      json.dump(self.logDict,jsonLog)
+    return crabExitCode #" ".join(subPStdOut)
 #########################
 if __name__ == "__main__":
   #commandLine parsing
